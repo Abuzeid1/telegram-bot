6b8a66c488ba2fa9ba14257e904d7dc21d9e8419
Ahmed Abuzeid <abuzeid9919@gmail.com>
2021-04-28 20:19:08 +0200

    v1

---
+++ mongo.js
@@ -0,0 +1,214 @@
+require('dotenv').config()
+const {MongoClient} = require("mongodb")
+const uri = process.env.uri
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+module.exports.inline = (com, arr, div)=>{
+  arr.sort()
+  let markup = []
+  if(!com){com =""}
+  if(div){
+    let x = -1;
+    let counter = 0;
+    for(let n of arr){
+      if(counter%div===0){
+        markup.push([])
+        x += 1
+      }
+      counter += 1
+      markup[x].push({text:n,callback_data:com+","+n})}
+  }else{
+    for(let n of arr){
+    markup.push([{text:n,callback_data:com+","+n}])
+  }}
+
+  if(com != "get"){com = com.split(",")
+    com.pop()
+    com = com.toString()
+    if(com===""){com ="0"}
+    
+      markup.push([{text:"<< Back",callback_data:com}])}
+    
+    
+  return markup
+}
+
+//media group
+
+module.exports.media = (obj)=>{
+  let group = []; 
+  for(let n of obj.arr){
+     group.push({type: obj.type, media: n})
+  }
+  return group
+}
+
+//construct media group
+module.exports.cmedia =(arr)=>{
+  let resultarr=[];
+
+  for(let n of arr){
+   let index =  resultarr.findIndex((el)=>{return el.type == n[1]})
+   if(index === -1){
+     resultarr.push({arr: [n[0]], type: n[1]})
+   }else{
+    resultarr[index].arr.push( n[0])
+   }
+  }
+  return resultarr
+}
+
+//buttons of the lists type && number
+
+module.exports.list =async (com, datarr)=>{
+  
+    await client.connect();
+  const db = client.db(datarr[0]);
+    //nums
+  if(datarr[1]){
+    const dbco =  db.collection(datarr[1]);
+    const nums =  await dbco.find({},{_id:1}).map(function(item){ return item._id; }).toArray();
+    return  this.inline(com, nums, 3)
+    //types
+  }else{
+    const types = await db.listCollections().map(function(item){ return item.name; }).toArray();
+    return  this.inline(com, types)
+  }
+  }
+
+//add new document
+module.exports.write = async(datarr, files) => {
+  
+    await client.connect();
+    const db =   client.db(datarr[0]);
+    const dbco =  db.collection(datarr[1]);
+    let doc =await  dbco.findOne({_id: datarr[2]})
+    
+    if(!doc){
+      await dbco.insertOne({_id: datarr[2],arr: files})
+    
+      await this.new(datarr).then()
+      return true
+    }else{
+
+      }
+      return false
+  }
+//  this.write(["47", "10", "6"], [1,2,3,4,5,6,7,8,9,10,11])
+
+//new 
+
+module.exports.new= async(xmo)=>{
+  
+  await client.connect()
+  const db =   client.db("new");
+  const dbco =  db.collection("new");
+  let farr = []
+ 
+  if(!xmo){
+    let doc = await dbco.findOne({_id: "new"})
+    
+    for(let n of doc.arr){
+      let x = n.toString()
+      farr.push([{text: x.replaceAll(",", " "), callback_data:"get," +x}])
+    }return farr
+  }
+  else{
+    
+      dbco.updateOne({_id: "new"},{$pop:{arr: 1}})
+      dbco.updateOne({_id: "new"},{$push: {arr:{$each: [xmo],$position: 0}}})
+      return
+      }
+  
+  
+}
+
+
+
+module.exports.permission =async(id, arr)=>{
+  
+  await client.connect();
+  const db = client.db("permission");
+  const dbco =  db.collection(id.toString());
+  const doc = await dbco.findOne({_id: arr[0]});
+  
+  if(doc){
+    let  check = doc.arr;
+    if(check.indexOf(arr[1]) != -1){
+      return true
+    }
+    return false
+  }
+  return false
+}
+//granting permission
+
+module.exports.grantpermission = async(id , arr)=>{
+  await client.connect();
+  const db = client.db("permission");
+  const dbco =  db.collection(id.toString());
+  const doc = await dbco.findOne({_id: arr[0]});
+  if(doc){
+      dbco.updateOne({_id: arr[0]},{$push: {arr: arr[1]}} )
+  }else{  
+    await dbco.insertOne({_id: arr[0], arr: [arr[1]]})
+  }
+  this.retrieve(id, arr).then((val)=>{this.write(arr, val)})
+  return true
+}
+
+
+module.exports.start =(com)=>{
+  let subjects = ['bacteriology', 'clinical', 'parasitology', 'pathology', 'pharmacology', 'poultry', 'virology']
+  let arr = this.inline(com, subjects, 2)
+  if(com ==="get"){arr.push([{text: "add", callback_data: "add"}, {text: "new", callback_data: "new"}])};
+  return arr
+}
+
+module.exports.count=async (arr)=>{
+ try {
+  await client.connect();
+  const db = await client.db(arr[0]);
+  const dbco =  db.collection(arr[1]);
+  let c = await dbco.countDocuments()
+  return c+1
+}finally{
+  }
+}
+
+
+
+module.exports.copy = async(arr, files, id)=>{
+  await client.connect();
+  const db =  client.db("copy");
+    const dbco = db.collection(id.toString())
+    if(! await dbco.findOne({arr: arr})){
+      dbco.insertOne({arr: arr, files: files})
+      return true
+    }
+    return false
+}
+ 
+
+module.exports.retrieve = async(id , arr)=>{
+  await client.connect();
+  const db =  client.db("copy");
+  const dbco = db.collection(id.toString())
+  let doc = await dbco.findOne({arr: arr})
+  return doc.files
+}
+
+module.exports.read = async(arr)=>{
+
+  await client.connect();
+  const db =  client.db(arr[0]);
+  const dbco = db.collection(arr[1])
+  let doc = await dbco.findOne({_id: arr[2]})
+  return doc.arr
+}
+
+
---
+++ server.js
@@ -0,0 +1,227 @@
+require('dotenv').config()
+
+let mongo = require("./mongo.js")
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser'); 
+
+//telegrambot
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.token;
+const bot = new TelegramBot(token);
+
+//new 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/bot' , (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// command global variable 
+var command ;
+var filearr = []
+let msd;
+//(query)
+bot.on('callback_query', (query)=>{
+  
+  const data = query.data;
+  const chatId = query.message.chat.id;
+  let msgId = query.message.message_id
+  const datarr = data.split(",")
+  
+  command = datarr
+  
+  if(data ==="0" || data==="get"){bot.editMessageReplyMarkup({inline_keyboard : mongo.start("get")},{chat_id: chatId, message_id: msgId})
+  }//new button
+  else if(data=== "new"){
+    mongo.new().then((val)=>{
+      
+      bot.editMessageReplyMarkup({inline_keyboard: val},{chat_id: chatId, message_id: msgId})
+    })
+  }//add commands
+  else if(data==="add"){
+    bot.editMessageReplyMarkup({inline_keyboard : mongo.start(data)},{chat_id: chatId, message_id: msgId})
+  }else if(datarr[0]==="add"){
+      if(datarr.length===2){
+        datarr.shift()
+        
+        mongo.list(data,datarr).then((value)=>{
+          value.splice(value.length-1, (0), [{text:"another", callback_data:"addx," + datarr.toString()}])
+         
+          bot.editMessageReplyMarkup({inline_keyboard :value},{chat_id: chatId, message_id: msgId})
+        })
+      
+      }else if(datarr.length===3){
+        datarr.shift()
+        mongo.count(datarr).then((value)=>{
+          let mark = mongo.inline(data, [value.toString()])
+          mark.splice(mark.length-1, (0), [{text:"another", callback_data:"addx," + datarr.toString()}])
+          bot.editMessageReplyMarkup({inline_keyboard :mark},{chat_id: chatId, message_id: msgId})
+        })
+      }else if(datarr.length===4){
+      bot.editMessageReplyMarkup({inline_keyboard :[[]]},{chat_id: chatId, message_id: msgId});
+      bot.sendMessage(chatId, "Send your files")
+    }}
+    
+  else if(datarr[0] === "addfiles"){
+    datarr.shift()
+    let files = mongo.cmedia(filearr)
+   
+    filearr= []
+    
+    bot.editMessageReplyMarkup({inline_keyboard :[[]]},{chat_id: chatId, message_id: msgId})
+    mongo.permission(chatId, datarr).then((value)=>{
+      if(value){
+        
+        mongo.write(datarr, files).then((val)=>{
+          if(!val){bot.sendMessage(chatId, "file already exist")}else{
+            datarr.pop()
+            datarr.unshift("add")
+            bot.sendMessage(chatId, "Done it will take some time untill files appear",
+             {reply_markup: {inline_keyboard :[[{text: "add one more", callback_data: datarr.toString()}],
+            [{text: "cansel", callback_data: "noanswer"}]]}})
+          }
+        })
+      }else{
+        bot.sendMessage(chatId, "Done it will take some time untill files appear");
+        bot.forwardMessage(process.env.userId, chatId, msd);
+
+        mongo.copy(datarr, files, chatId).then((value)=>{})
+        
+        let cd = ["gper", chatId,datarr, query.from.first_name, query.from.last_name]
+        cd = JSON.stringify(cd)
+        
+        bot.sendMessage(process.env.userId, "grant permission " + cd,
+         {reply_markup: {inline_keyboard: [
+         [{text: "yes", callback_data:  cd}],
+         [{text: "no", callback_data: "noanswer"}]]}} )
+      }
+    })
+  }else if(datarr[0]==='["gper"'){
+   
+  
+    
+    bot.editMessageReplyMarkup({inline_keyboard :[[]]},{chat_id: chatId, message_id: msgId});
+    let x = JSON.parse(data)
+    x.shift()
+  
+    //datarr.shift()
+    let pid = x[0];
+    let m  = x[1]
+    
+    mongo.grantpermission(pid, m).then((value)=>{});
+    
+
+  }else if(datarr[0]==="noanswer"){
+     bot.editMessageReplyMarkup({inline_keyboard :[[]]},{chat_id: chatId, message_id: msgId});
+     
+  }else if(datarr[0]==="addx"){
+     bot.editMessageReplyMarkup({inline_keyboard :[[]]},{chat_id: chatId, message_id: msgId})
+     bot.sendMessage(chatId, "send the new one name")
+  }//getting commands
+  else if(datarr[0]==="get"){
+    if(datarr.length === 2){
+      datarr.shift()
+        mongo.list(data,datarr).then((value)=>{
+          bot.editMessageReplyMarkup({inline_keyboard :value},{chat_id: chatId, message_id: msgId})
+        })
+    }else if(datarr.length === 3){
+      datarr.shift()
+        mongo.list(data,datarr).then((value)=>{
+          bot.editMessageReplyMarkup({inline_keyboard :value},{chat_id: chatId, message_id: msgId})
+        })
+    }else if(datarr.length === 4){
+      console.log(query.from.first_name, query.from.last_name)
+      datarr.shift()
+      mongo.read(datarr).then((val)=>{
+        for(let x of val){
+
+          if(x.type==="voice"){
+            for(let vo of x.arr){
+              bot.sendVoice(chatId, vo)
+            }
+          }else{
+             let mm = mongo.media(x)
+             let sender = (arr)=>{
+               if(arr.length<11){
+                bot.sendMediaGroup(chatId, arr)
+               }else{
+                 let arr2 = arr.splice(10);
+                 bot.sendMediaGroup(chatId, arr)
+                 sender(arr2)
+               }
+             }
+             
+             sender(mm)            
+          }
+
+         
+        }
+      })
+    }
+  }
+  
+ 
+})
+
+
+
+bot.on('message', (msg)=>{ 
+   
+  const chatId = msg.chat.id
+  const text = msg.text;
+  
+  
+   if(text === "/start"){
+    bot.sendMessage(chatId,"start",{reply_markup: {inline_keyboard: mongo.start("get"),one_time_keyboard: true}})
+}
+ else if(command[0]=== "add" && command.length === 4){
+   msd = msg.message_id;
+ 
+   if(filearr.length===0){
+    
+    var calldat = command.slice(1)
+    
+    calldat.unshift("addfiles")
+    calldat = calldat.toString()
+    bot.sendMessage(chatId, "push only when all the files have been uploaded", 
+      {reply_markup: {inline_keyboard: [[{text: "confirm", callback_data: calldat}],[{text: "cansel", callback_data: "noanswer"}]],one_time_keyboard: true}}
+    )
+  }
+  
+    if(msg.photo){
+      filearr.push([msg.photo[1].file_id, "photo"]) 
+    }
+    else if(msg.video){
+      filearr.push([msg.video.file_id, "video"])
+    }
+    else if(msg.document){
+      filearr.push([msg.document.file_id, "document"])
+    }else if(msg.audio){
+      filearr.push([msg.audio.file_id, "audio"])
+    }else if(msg.voice){
+      filearr.push([msg.voice.file_id, "voice"])
+    }
+   
+ }else if(command[0]==="addx"){
+   
+   command[0] = "add"
+   command.push(text)
+   bot.sendMessage(chatId, "the new one is "+ text,
+   {reply_markup: {inline_keyboard: [[{text: "confirm", callback_data: command.toString()}],[{text: "cansel", callback_data: "noanswer"}]]}})
+ }//start command
+ else(bot.sendMessage(chatId,"start",{reply_markup: {inline_keyboard: mongo.start("get"),one_time_keyboard: true}}))
+  })
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+
+
+
+
+
+
