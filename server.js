600f9d880de01c8ec09ea5e3419d1c2567727d77
Ahmed Abuzeid <abuzeid9919@gmail.com>
2021-04-28 20:59:30 +0200

    v1

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const token = process.env.token;
 const bot = new TelegramBot(token);
 
 //new 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
   app.get('/', (req, res)=>{
     res.send("hello working")
   })
